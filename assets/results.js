5776cc72633201e816807c6460e4f11348864367
skullkid4200 <jackeinhorn123@gmail.com>
2023-01-15 14:55:37 -0500

    pushing up the tmdb api functionality

--- assets/results.js
+++ assets/results.js
@@ -1,27 +1,26 @@
 
 var v1El = document.querySelector('#video-1');
 
-var v1Input = 'hunger games';
+var v1Input;
+var v2Input;
+var v3Input;
+var obj = localStorage.getItem('imdbID');
 var v1id;
 var v1Url;
 
+var number = Math.trunc(Math.random() * 500) + 1;
+
 var trailerAPI = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&key=${ytApiKey}&q=${v1Input}_official_trailer`;
-
-<<<<<<< HEAD:assets/results.js
-=======
-
-// // Movie API
-// var movieName = "cabin in the woods";
-// var movieID = "tt1259521"
-// //tmdb
-// var movieAPI = `https://api.themoviedb.org/3/movie/550?api_key=32d3014b4c0bf100be1034a9bed533d5`
-// //omdb
->>>>>>> main:assets/script.js
+var tmdbAPI = `https://api.themoviedb.org/3/movie/${obj}/similar?api_key=${tmdbApiKey}&language=en-US&page=${number}`;
 
 
-function getVideo1() {
 
-  fetch(trailerAPI, {
+function getSimilarMovies() {
+
+  obj = localStorage.getItem('imdbID');
+  console.log(obj);
+
+  fetch(tmdbAPI, {
     method: 'GET',
     // mode: 'no-cors',
     credentials: 'same-origin',
@@ -31,106 +30,72 @@ function getVideo1() {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
-          v1id = data.items[0].id.videoId;
-          displayV1(v1id);
+          v1Input = data.results[0].title;
+          v2Input = data.results[1].title;
+          v3Input = data.results[2].title;
+          console.log(v1Input + v2Input + v3Input);
         });
-      } else {
-        console.log(response);
-        alert('Error: ' + response.statusText);
       }
     })
-};
+}
 
-
-getVideo1();
-
-
-function displayV1(v1id) {
-  // if (!v1id) {
-  //   v1El.textContent = 'Error: Video Not Found.';
-  //   return;
-  // }
-
-  // console.log(v1id);
-
-  v1El.innerHTML = `
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/${v1id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    `;
-
-};
-
-
-<<<<<<< HEAD:assets/results.js
-// // return 3 similar movies with movie ID
-fetch(`https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=32d3014b4c0bf100be1034a9bed533d5&language=en-US&page=1&`)
-  .then(response => response.json())
-  .then(json => console.log(json))
-=======
-// // MOVIE FUNCTIONS // 
-
-// //load random movie posters on landing page upon opening the website 
-// // window.onload = function () {
-// //   showPosters()
-// // }
-
-// // showPosters()
-// // 
-
-// //   });
+getSimilarMovies()
 
 
 
-// // return a response by searching a movie name
-// fetch(`http://www.omdbapi.com/?apikey=f9b9102e&t=${movieName}`)
-//   .then(response => response.json())
-//   .then(json => console.log(json))
-
-// //pull out the ID of the movie that was searched, (index 23 in this instance)
-// json.forEach((repo) => {
-
-// });
 
 
-// // return 3 similar movies with movie ID
-// fetch(`https://api.themoviedb.org/3/movie/${movieID}/similar?api_key=32d3014b4c0bf100be1034a9bed533d5&language=en-US&page=1&`)
-//   .then(response => response.json())
-//   .then(json => console.log(json))
->>>>>>> main:assets/script.js
 
-// function changePage() {
 
-//   document.location.href = "file:///C:/Users/Jack/Desktop/BOOTCAMP_WORK/FIRST%20GROUP%20PROJECT/Atomic-Cats-Project-1/results.html";
 
-// }
+// function getVideo1() {
 
-// searchBtn.addEventListener("click", searchMovie, changePage);
+//   fetch(trailerAPI, {
+//     method: 'GET',
+//     // mode: 'no-cors',
+//     credentials: 'same-origin',
+//     redirect: 'follow',
+//   })
+//     .then(function (response) {
+//       if (response.ok) {
+//         response.json().then(function (data) {
+//           console.log(data);
+//           v1id = data.items[0].id.videoId;
+//           displayV1(v1id);
+//         });
+//       } else {
+//         console.log(response);
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+// };
 
-<<<<<<< HEAD:assets/results.js
-=======
-// // Genres to choose from
-// // ==============================
-// // Action
-// // Adventure
-// // Animation
-// // Biography
-// // Comedy
-// // Crime	
-// // Documentary
-// // Drama
-// // Family
-// // Fantasy
-// // Film Noir
-// // History	
-// // Horror
-// // Music
-// // Musical
-// // Mystery
-// // Romance
-// // Sci-Fi	
-// // Short Film
-// // Sport
-// // Superhero
-// // Thriller
-// // War
-// // Western
->>>>>>> main:assets/script.js
+
+// getVideo1();
+
+
+// function displayV1(v1id) {
+//   // if (!v1id) {
+//   //   v1El.textContent = 'Error: Video Not Found.';
+//   //   return;
+//   // }
+
+//   // console.log(v1id);
+
+//   v1El.innerHTML = `
+//     <iframe width="560" height="315" src="https://www.youtube.com/embed/${v1id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//     `;
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
