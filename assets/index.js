402ec643a92c17b4a25e9d41195977fcebdd14bd
Codename O'Connell <oconnellcoder@gmail.com>
2023-01-18 19:15:16 -0500

    condensed 6 functions into 2 functions for simplicity, cleaned up code and fixed minor errors

--- assets/index.js
+++ assets/index.js
@@ -4,16 +4,10 @@ var movie;
 var movie1ID;
 
 
-// Input Area has cursor ready for user
 searchField.focus();
 
-// Movie API testing
-var movieName = "cabin in the woods";
-var movieID = "tt1259521"
-//tmdb
+
 var tmdbAPI = `https://api.themoviedb.org/3/movie/550?api_key=32d3014b4c0bf100be1034a9bed533d5`
-//omdb
-var omdbAPI = `https://www.omdbapi.com/?apikey=f9b9102e&t=${movieName}`
 
 
 function searchMovie() {
@@ -21,8 +15,6 @@ function searchMovie() {
   input.value = "";
 
   fetch(`https://www.omdbapi.com/?apikey=f9b9102e&t=${movie}`, {
-    method: 'GET',
-    // mode: 'no-cors',
     credentials: 'same-origin',
     redirect: 'follow',
   })
@@ -54,18 +46,6 @@ function changePage() {
 
 
 searchBtn.addEventListener("click", searchMovie, changePage);
-
-// MOVIE FUNCTIONS //
-
-//load random movie posters on landing page upon opening the website
-// window.onload = function () {
-//   showPosters()
-// }
-
-// showPosters()
-//
-
-//   });
 
 
 
